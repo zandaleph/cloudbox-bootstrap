1ad5fd96ad6bf8dc6b0b4103a068be3c773dccaf
Zack Spencer <zandaleph@gmail.com>
2013-02-08 20:31:30 -0800

    Installing ruby and the aws-sdk in bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,6 +4,5 @@ SCRIPT_PATH=`cd "\`dirname $0\`"; pwd`
 BASE_PATH=`cd $SCRIPT_PATH/..; pwd`
 RVM_PATH="$BASE_PATH/rvm"
 
-# Install RVM (not sure if this is local to the directory yet)
-# this may involve setting $HOME temporarily
-\curl -L https://get.rvm.io | HOME=$BASE_PATH rvm_ignore_rvmrc=1 bash -s -- stable --path $RVM_PATH --ignore-dotfiles --ruby
+# Install RVM, ruby, and the aws-sdk gem
+\curl -L https://get.rvm.io | HOME=$BASE_PATH bash -s -- stable --path $RVM_PATH --ignore-dotfiles --ruby --gems=aws-sdk
